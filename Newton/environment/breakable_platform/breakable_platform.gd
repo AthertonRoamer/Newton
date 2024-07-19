da439d0e466b807303cb1fed6d6f53d9d81b6faf
AthertonRoamer <111521556+AthertonRoamer@users.noreply.github.com>
2024-07-19 12:17:08 -0500

    Added breakable platform

---
+++ Newton/environment/breakable_platform/breakable_platform.gd
@@ -0,0 +1,28 @@
+class_name BreakablePlatform
+extends StaticBody2D
+
+@export var damage_needed_to_break : int = 15
+var broken : bool = false
+var damage_taken : int = 0:
+	set(v):
+		damage_taken = v
+		if damage_taken > damage_needed_to_break and not broken:
+			call_deferred("break_platform")
+
+
+func _ready() -> void:
+	add_to_group("damageable")
+	
+	
+func take_damage(damage : int, _damage_type : String) -> void:
+	damage_taken += damage
+	
+	
+func break_platform() -> void:
+	broken = true
+	$CollisionShape2D.disabled = true
+	#play break animation
+	queue_free()
+	
+	
+	
