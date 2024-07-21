c82bc1d491df675d237fced4019d39fbc3a0d409
AthertonRoamer <111521556+AthertonRoamer@users.noreply.github.com>
2024-07-20 22:56:30 -0500

    Added lightning

--- Newton/player/spells/lightning_spell/lightning_spell.gd
+++ Newton/player/spells/lightning_spell/lightning_spell.gd
@@ -2,6 +2,15 @@ class_name LightningSpell
 extends Spell
 
 @export var lightning_scene : PackedScene
+@export var lightning_storm_scene : PackedScene 
+
+@export var base_damage : float = 50
+@export var max_damage : float = 200
+@export var min_damage : float = 10
+
+@export var base_strike_count : int = 3
+@export var max_strike_count : int = 10
+
 
 func cast() -> void:
 	spawn_lightning()
@@ -9,4 +18,13 @@ func cast() -> void:
 
 
 func spawn_lightning() -> void:
-	pass
+	var damage : int = max(min(base_damage * charge_time * charge_time, max_damage), min_damage)
+	var strike_count : int = min(round(base_strike_count * charge_time * charge_time), max_strike_count)
+	print("lightning damage: " + str(damage))
+	
+	var storm : LightningStorm = lightning_storm_scene.instantiate()
+	storm = storm as LightningStorm
+	storm.global_position = player.get_global_mouse_position()
+	storm.lightning_damage = damage
+	storm.strike_count = strike_count
+	Main.world.object_holder.add_child(storm)
